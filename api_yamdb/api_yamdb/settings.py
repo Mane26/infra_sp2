262ec7abef8633603bcafdc38d5374eb5cc495b3
Meline Sarkisyan <s.m.m21-08-90@mail.ru>
2023-02-12 17:12:29 +0300

    1ревью

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,11 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '158.160.8.109',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 INSTALLED_APPS = [
